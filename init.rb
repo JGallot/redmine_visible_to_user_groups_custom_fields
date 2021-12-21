194e366bc8e5cb10d988b1a1a5b555545d59a8fe
Frederico Camara <frederico.camara@serpro.gov.br>
2021-12-21 14:07:28 -0300

    Initial commit

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_restrict_cfs_by_user_cfs do
+  name 'Redmine Restrict Cfs By User Cfs plugin'
+  author 'Author name'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end
